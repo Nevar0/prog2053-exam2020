8e5ce77422e9b8d5f8eda32264865c7ba946bba5
Rihards <rihardsu@stud.ntnu.no>
2020-12-17 12:22:25 +0100

    edit user finishedgit add .

--- oppgave/www/js/components/edit-user.js
+++ oppgave/www/js/components/edit-user.js
@@ -7,7 +7,68 @@ class EditUser extends LitElement {
     };
   }
 
-  // din kode her
+  // CODE
+
+
+  //Uses updateUser.php to update uer data
+  updateUser(e) {
+
+    //gets data from HTML
+    const dataForm = new FormData(e.target.form);
+    console.log(e)
+    
+    fetch('api/updateUser.php', {
+     method: 'POST',
+     body: dataForm
+    }).then(res=>res.json())
+      .then(data=>{
+        if (data.status=='success') {
+            console.log("User updated");
+
+        } else {
+            console.log("User not updated");
+        }
+
+      })
+  }
+
+
+  render() {
+    return html`
+
+    <head>
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    </head>
+    <form onsubmit="javascript: return false;" id="userForm" method="POST">
+      <div>
+        <div class="form-group pt-3 ml-5" style="width: 20rem;">
+          <label for="email">Email</label>
+          <input class="form-control" id="uname" name="uname" type="text" value="${this.user.uname}" required>
+          <input type="hidden" id="uid" name="uid" value="${this.user.uid}">
+        </div>
+        <div class="form-group pt-1 ml-5" style="width: 20rem;">
+          <label for="firstName">First Name</label>
+          <input class="form-control" id="firstName" name="firstName" type="text" value="${this.user.firstName}" required>
+        </div>
+        <div class="form-group pt-1 ml-5" style="width: 20rem;">
+          <label for="lastName">Last Name</label>
+          <input class="form-control" id="lastName" name="lastName" type="text" value="${this.user.lastName}" required>
+        </div>
+        <div class="form-group pt-1 ml-5" style="width: 20rem;">
+          <label for="oldpwd">Old Password</label>
+          <input type="password" class="form-control" id="oldpwd" name="oldpwd" type="text" value="">
+        </div>
+        <div class="form-group pt-1 ml-5" style="width: 20rem;">
+          <label for="newpwd">New Password</label>
+          <input type="password" class="form-control" id="pwd" name="pwd" type="text" value="">
+        </div>
+      </div>
+      <input type="submit" @click=${this.updateUser} id="submitForm" name="Save" class="btn btn-info mt-4 ml-2" value="Save"></input>
+    </form>
+    
+    `;
+  }
+
 
 }
 customElements.define('edit-user', EditUser);
